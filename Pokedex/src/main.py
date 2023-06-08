55cf783ff88bbd73bf3decbcb0233db4761a6a84
Hector <hectorvallsmira2003@gmail.com>
2023-06-08 19:45:33 +0200

    [MOD] Ultimas modificaciones

--- Pokedex/src/main.py
+++ Pokedex/src/main.py
@@ -10,7 +10,7 @@ import glob
 import random
 
 
-TOKEN = 'MTA3Nz2NzY0NTgfefwfefwfFst0oDxMDgIY'
+TOKEN = 'MTA3NzY0MzU2NzY0NTg2ODExNA.Gsh27z.8gfuQo9X3u-_z4CVKr2TvkX0OSFst0oDxMDgIYmmmm'
 
 intents = discord.Intents.all()
 
@@ -35,10 +35,15 @@ async def pokemon(ctx, numero):
 
     url = f'http://localhost:2310/hector/pokemon/{numero}'
     response = http.request("GET", url)
-
     pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
-    await pokemonEmbed(ctx, pokemon, True)
 
+
+    try:
+        await pokemonEmbed(ctx, pokemon, True)
+    except Exception as e:
+        await ctx.send("There was an error, check that your number is available, or try again later")
+
+    
 @client.command()
 async def pokemonName(ctx, name):
     channel = client.get_channel(ctx.channel.id)
@@ -48,31 +53,36 @@ async def pokemonName(ctx, name):
     url = f'http://localhost:2310/hector/pokemon/name/{name}'
     response = http.request("GET", url)
     pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
-    embed = await pokemonEmbed(ctx, pokemon, False) 
-    await ctx.send(embed=embed)   
-
-
-regiones = ["Kanto", "Jotho", "Hoenn", "Sinnoh","Teselia", "Kalos", "Galar"]
+    try:
+        embed = await pokemonEmbed(ctx, pokemon, False) 
+        await ctx.send(embed=embed) 
+    except Exception as e:
+        await ctx.send("There was an error, check that you spelled correctly, or try again later")
+          
 
 @client.command()
 async def info(ctx):
     des = """
-  Comandos de TestBot\n
-  > Prefix:  .\n
+  Comandos de TestBot
+  > Prefix:  .
 
-  > pokemon <number>: Searches the Pokemon related to the number\n
+  > pokemon <number>: Searches the Pokemon related to the number
 
-  > pokemonName <name>: Searches a Pokemon by name\n
+  > pokemonName <name>: Searches a Pokemon by name
 
-  > pokeRegion <regionName>: Searches all the Pokemons from said region\n
+  > pokeRegion <regionName>: Searches all the Pokemons from said region
 
-  > register: Saves your username so you can follow your pokedex status\n
+  > register: Saves your username so you can follow your pokedex status
 
-  > catch <number>: Registers the Pokemon related to the number wrote\n
+  > catch <number>: Registers the Pokemon related to the number wrote
 
-  > caughtCount: Tells you your current Pokedex status\n
+  > caughtCount: Tells you your current Pokedex status
 
-  > caughtRegion: Same as above but from an specific region\n
+  > caughtRegion: Same as above but from an specific region
+
+  > leftInTotal: Displays all the Pokemons left to catch
+
+  > leftInRegion: Displays the Pokemons left to catch in a specific Region
 
   > effectiveness <typeName> <objectivePokemonName>: Tells you how effective is a Type against a Pokemon\n
   
@@ -122,18 +132,13 @@ async def pokemonEmbed(ctx,pokemon,send):
         return embed
 
 
-@client.command()
-async def pokeRegion(ctx, region):
-
-    http = urllib3.PoolManager()
-
-    url = f'http://localhost:2310/hector/pokemon/region/{region}'
-    response = http.request("GET", url)
-    pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+async def listOfEmbeds(ctx, list):
+    try:
+        print(list[0])
+        message = await ctx.send(embed= await pokemonEmbed(ctx, list[0], False)) 
+    except Exception as e:
+        await ctx.send("There was an error, check that you spelled correctly, or try again later")
     
-    print(type(pokemon) is list)
-
-    message = await ctx.send(embed= await pokemonEmbed(ctx, pokemon[0], False))
     await message.add_reaction('⏮')
     await message.add_reaction('◀')
     await message.add_reaction('▶')
@@ -152,18 +157,18 @@ async def pokeRegion(ctx, region):
     while True:
         if str(reaction) == '⏮':
             i = 0
-            await message.edit(embed = await pokemonEmbed(ctx, pokemon[i], False))
+            await message.edit(embed = await pokemonEmbed(ctx, list[i], False))
         elif str(reaction) == '◀':
             if i > 0:
                 i -= 1
-                await message.edit(embed = await pokemonEmbed(ctx, pokemon[i], False))
+                await message.edit(embed = await pokemonEmbed(ctx, list[i], False))
         elif str(reaction) == '▶':
-            if i < len(pokemon):
+            if i < len(list):
                 i += 1
-                await message.edit(embed = await pokemonEmbed(ctx, pokemon[i], False))
+                await message.edit(embed = await pokemonEmbed(ctx, list[i], False))
         elif str(reaction) == '⏭':
-            i = len(pokemon)-1
-            await message.edit(embed = await pokemonEmbed(ctx, pokemon[i], False))
+            i = len(list)-1
+            await message.edit(embed = await pokemonEmbed(ctx, list[i], False))
         
         try:
             reaction, user = await client.wait_for('reaction_add', timeout = 15.0, check = check)
@@ -172,6 +177,18 @@ async def pokeRegion(ctx, region):
             break
 
     await message.clear_reactions()
+
+
+@client.command()
+async def pokeRegion(ctx, region):
+
+    http = urllib3.PoolManager()
+
+    url = f'http://localhost:2310/hector/pokemon/region/{region}'
+    response = http.request("GET", url)
+    pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+    await listOfEmbeds(ctx, pokemon)
+    
 
 
 @client.command()
@@ -208,7 +225,7 @@ async def catch(ctx, pokeCaught):
         userData = str(ctx.author).split('#')
         poke = '{{"id": {}}}'.format(pokeCaught)
         user = '{{"id":{}, "name": "{}", "pokemonDTO":[{}] }}'.format(userComplete.id, userData[0], poke)
-        print(user)
+
         url = f'http://localhost:2310/hector/users'
         response = http.urlopen(method='PUT', url=url, body=user , headers={'Content-Type': 'application/json'})
         #embed = await pokemonEmbed(ctx, pokemon, False) 
@@ -241,6 +258,41 @@ async def caughtCount(ctx):
         else:
              await ctx.send("You caught every single Pokemon out there, you are awesome")
 
+@client.command()
+async def leftInTotal(ctx):
+    http = urllib3.PoolManager()
+
+    userData = str(ctx.author).split('#')
+    url = 'http://localhost:2310/hector/users/name/{}'.format(userData[0])
+    response = http.request("GET", url)
+    userComplete = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+
+    if response.status != 200:
+        await ctx.send("Error, check you are registered and that the command is correct") 
+    else:
+        url = f'http://localhost:2310/hector/pokemon?withType=True'
+        response = http.request("GET", url)
+        pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+
+        temp = []
+        for poke in userComplete.pokemonDTO:
+            temp.append(poke.id)
+
+        pokeLeft =  []
+        for poke in pokemon:
+            found = False
+            for caught in temp:
+                if poke.id == caught:
+                    found = True
+            
+            if not found:
+                pokeLeft.append(poke)
+        
+        if len(pokeLeft) == 0:
+            await ctx.send("There's no Pokemons left to catch on that region")
+        else:
+            await listOfEmbeds(ctx, pokeLeft)
+
 
 @client.command()
 async def caughtRegion(ctx, region):
@@ -251,7 +303,6 @@ async def caughtRegion(ctx, region):
     response = http.request("GET", url)
     userComplete = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
 
-    print(len(userComplete.pokemonDTO))
     if response.status != 200:
         await ctx.send("Error, check you are registered and that the command is correct") 
     else:
@@ -270,6 +321,45 @@ async def caughtRegion(ctx, region):
         else:
              await ctx.send("You caught every single Pokemon of {}, you are awesome".format(region))
     
+@client.command()
+async def leftInRegion(ctx, region):
+    http = urllib3.PoolManager()
+
+    userData = str(ctx.author).split('#')
+    url = 'http://localhost:2310/hector/users/name/{}'.format(userData[0])
+    response = http.request("GET", url)
+    userComplete = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+
+    if response.status != 200:
+        await ctx.send("Error, check you are registered and that the command is correct") 
+    else:
+        url = f'http://localhost:2310/hector/pokemon/region/{region}'
+        response = http.request("GET", url)
+        pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+
+        temp = []
+        for poke in userComplete.pokemonDTO:
+            if poke.region.name == region:
+                temp.append(poke.id)
+
+        pokeLeft =  []
+        print(temp)
+        for poke in pokemon:
+            found = False
+            for caught in temp:
+                if poke.id == caught:
+                    found = True
+            
+            if not found:
+                pokeLeft.append(poke)
+
+        print(len(pokeLeft))
+
+        if len(pokeLeft) == 0:
+            await ctx.send("There's no Pokemons left to catch on that region")
+        else:
+            await listOfEmbeds(ctx, pokeLeft)
+
 
 
 @client.command()
@@ -279,32 +369,33 @@ async def effectiveness(ctx, typeU, pokemon):
     url = f'http://localhost:2310/hector/pokemon/name/{pokemon}'
     response = http.request("GET", url)
     pokemon = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
+
+    try:
+        types = []
+        for pokeType in pokemon.types:
+            types.append(pokeType.name) 
+    except Exception as e:
+        await ctx.send("There was an error, check that you spelled correctly, or try again later")
     
-    types = []
-    for pokeType in pokemon.types:
-        types.append(pokeType.name)
+    
 
 
     url = f'http://localhost:2310/hector/effects/attack/{typeU}'
     response = http.request("GET", url)
     effects = json.loads(response.data.decode("utf-8"), object_hook=lambda d: SimpleNamespace(**d))
-    print(len(effects))
-    print(len(types))
 
     finalEffect = 1
-    for effectType in types:
-        print(effectType)
-        for effectiveness in effects:
-            if effectiveness.defensiveType.name == effectType:
-                print(effectiveness.defensiveType.name)
-                finalEffect = finalEffect*effectiveness.effect
-                print(finalEffect)
+    try:
+        for effectType in types:
+            for effectiveness in effects:
+                if effectiveness.defensiveType.name == effectType:
+                    finalEffect = finalEffect*effectiveness.effect
+    except Exception as e:
+        await ctx.send("There was an error, check that you spelled correctly, or try again later")
+    
     
 
-    if response.status != 200:
-        await ctx.send("Error, check you are registered and that the command is correct") 
-    else:
-        await ctx.send("The {} type has a x{} effectiveness against {}".format(typeU, finalEffect, pokemon.name))
+    await ctx.send("The {} type has a x{} effectiveness against {}".format(typeU, finalEffect, pokemon.name))
 
 
 @client.command()
